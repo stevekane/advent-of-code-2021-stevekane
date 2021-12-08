5b32e91f6e657c38c3515e0cacc2b347aef8a67c
Steve Kane <kanesteven@gmail.com>
2021-12-08 13:54:27 -0600

    more tweak to tease apart data model in 8

--- 8/8.js
+++ 8/8.js
@@ -12,14 +12,16 @@ const digitpixels = [
   [1,1,1,1,1,1,1],
   [1,1,1,1,0,1,1],
 ]
-const renderSettings = [
-  { indices: [1,2,3,4], letter: "a" },
-  { indices: [7,14], letter: "b" },
-  { indices: [12,19], letter: "c" },
-  { indices: [22,23,24,25], letter: "d" },
-  { indices: [28,35], letter: "e" },
-  { indices: [33,40], letter: "f" },
-  { indices: [43,44,45,46], letter: "g" },
+const indexToLetter = ["a","b","c","d","e","f","g"]
+const letterToIndex = indexToLetter.reduce((m,l,i) => m.set(indexToLetter[i],i),new Map)
+const pixelIndices = [
+  [1,2,3,4],
+  [7,14],
+  [12,19],
+  [22,23,24,25],
+  [28,35],
+  [33,40],
+  [43,44,45,46]
 ]
 function render(digit,wirings) {
   const width = 7
@@ -30,7 +32,8 @@ function render(digit,wirings) {
   for (var i = 0; i < segments.length; i++) {
     let on = segments[i]
     let wiring = wirings[i]
-    let { indices, letter } = renderSettings[wiring]
+    let indices = pixelIndices[wiring]
+    let letter = indexToLetter[wiring]
     for (var j = 0; j < indices.length; j++) {
       let index = indices[j]
       buffer[index] = on ? letter : " "
