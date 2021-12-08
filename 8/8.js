6942f815fccfa7ecec39ca0a76fa564d6b7e6e9d
Steve Kane <kanesteven@gmail.com>
2021-12-08 17:03:12 -0600

    added complete solution for 8

--- 8/8.js
+++ 8/8.js
@@ -1,62 +1,67 @@
 const { readFileSync } = require("fs")
+const { toNat, add, fold, isSuperset, difference, permutationOf } = require("../utils")
+const path = process.argv[2]
+const options = { encoding: "utf8" }
+const input =
+  readFileSync(path, options)
+  .split("\n")
+  .map(i => 
+    i
+    .split("|")
+    .map(c => 
+      c
+      .trim()
+      .split(" ")))
 
-const digitpixels = [
-  [1,1,1,0,1,1,1],
-  [0,0,1,0,0,1,0],
-  [1,0,1,1,1,0,1],
-  [1,0,1,1,0,1,1],
-  [0,1,1,1,0,1,0],
-  [1,1,0,1,0,1,1],
-  [1,1,0,1,1,1,1],
-  [1,0,1,0,0,1,0],
-  [1,1,1,1,1,1,1],
-  [1,1,1,1,0,1,1],
-]
-const indexToLetter = ["a","b","c","d","e","f","g"]
-const letterToIndex = indexToLetter.reduce((m,l,i) => m.set(indexToLetter[i],i),new Map)
-const pixelIndices = [
-  [1,2,3,4],
-  [7,14],
-  [12,19],
-  [22,23,24,25],
-  [28,35],
-  [33,40],
-  [43,44,45,46]
-]
-function render(digit,wirings) {
-  const width = 7
-  const height = 7
-  const buffer = new Array(width * height).fill(" ")
-  const segments = digitpixels[digit]
-
-  for (var i = 0; i < segments.length; i++) {
-    let on = segments[i]
-    let wiring = wirings[i]
-    let indices = pixelIndices[wiring]
-    let letter = indexToLetter[wiring]
-    for (var j = 0; j < indices.length; j++) {
-      let index = indices[j]
-      buffer[index] = on ? letter : " "
-    }
+function toString(xs) {
+  var s = ""
+  for (let x of xs) {
+    s += x
   }
-  for (var i = 0; i < height; i++) {
-    buffer[i * width + width - 1] = "\n"
-  }
-  return buffer.join("")
+  return s
 }
 
-const idealWiring = [0,1,2,3,4,5,6]
-
-const panels = [
-  { digits: [0,3,5,7], wiring: idealWiring },
-  { digits: [2,4,6,8], wiring: idealWiring },
-  { digits: [1,3,3,7], wiring: idealWiring },
-  { digits: [9,6,3,0], wiring: idealWiring },
-]
-
-
-for (let panel of panels) {
-  for (let digit of panel.digits) {
-    console.log(render(digit,panel.wiring))
-  }
+function correctDigits([ signals, digits ]) {
+  const one   = new Set(signals.find(s => s.length === 2))
+  const seven = new Set(signals.find(s => s.length === 3))
+  const four  = new Set(signals.find(s => s.length === 4))
+  const eight = new Set(signals.find(s => s.length === 7))
+  const fives = signals.filter(s => s.length === 5).map(s => new Set(s)) // 2 3 5
+  const sixes = signals.filter(s => s.length === 6).map(s => new Set(s)) // 0 6 9
+  const bd    = difference(four,one)
+  const three = fives.find(l => isSuperset(l,one)) // only 3 contains one
+  const zero  = sixes.find(l => !isSuperset(l,bd)) // only 0 does not contain both b and d
+  const nine  = sixes.filter(f => f != zero).find(f => isSuperset(f,one))
+  const six   = sixes.find(f => f != zero && f != nine)
+  const d     = difference(eight,zero)
+  const b     = difference(bd,d)
+  const five  = fives.filter(f => f != three).find(f => isSuperset(f,b)) 
+  const two   = fives.find(f => f != three && f != five)
+  const signalPatterns = [
+    toString(zero),
+    toString(one),
+    toString(two),
+    toString(three),
+    toString(four),
+    toString(five),
+    toString(six),
+    toString(seven),
+    toString(eight),
+    toString(nine)
+  ]
+  return digits.reduce((s,d) => s+signalPatterns.findIndex(s => permutationOf(s,d)),"")
 }
+
+const uniqueLengths = [0,0,1,1,1,0,0,1,0]
+const occurrences = 
+  fold((s,[_,ds]) => 
+    s+fold((c,d) => 
+      c+uniqueLengths[d.length],0,ds),0,input)
+const sumOfAllDisplays = 
+  input
+  .map(correctDigits)
+  .map(toNat)
+  .reduce(add)
+
+console.log(occurrences)
+console.log(sumOfAllDisplays)
--- utils.js
+++ utils.js
@@ -1,4 +1,12 @@
-module.exports = { log, toNat, add, fold, map, scan, range }
+module.exports = { 
+  log, 
+  toNat, 
+  isSuperset, difference, symmetricDifference, union, 
+  permutationOf,
+  add, 
+  sum, 
+  fold, map, scan, range 
+}
 
 function log(n) {
   return console.log(n)
@@ -8,8 +16,61 @@ function toNat(n) {
   return parseInt(n)
 }
 
+function isSuperset(set,subset) {
+  for (let e of subset) {
+    if (!set.has(e)) {
+      return false
+    }
+  }
+  return true
+}
+
+function difference(a,b) {
+  let d = new Set(a)
+  for (let e of b) {
+    d.delete(e)
+  }
+  return d
+}
+
+function symmetricDifference(a,b) {
+  let d = new Set(a)
+  for (let e of b) {
+    if (d.has(e)) {
+      d.delete(e)
+    } else {
+      d.add(e)
+    }
+  }
+  return d
+}
+
+function union(a,b) {
+  let u = new Set(a)
+  for (let e of b) {
+    u.add(e)
+  }
+  return u
+}
+
+function permutationOf(xs,ys) {
+  if (xs.length != ys.length)
+    return false
+
+  for (let x of xs) {
+    if (ys.indexOf(x) < 0) {
+      return false
+    }
+  }
+  return true
+}
+
 function add(a,b) {
   return a + b
+}
+
+function sum(xs) {
+  return fold(add,0,xs)
 }
 
 function fold(f,y0,xs) {
