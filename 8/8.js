de4a86a7065e2623c8740b708120c397e4edeb08
Steve Kane <kanesteven@gmail.com>
2021-12-08 13:17:24 -0600

    cleaned up representation of pixels in problem 8

--- 8/8.js
+++ 8/8.js
@@ -4,6 +4,15 @@ function renderSegments(segments) {
   const width = 7
   const height = 7
   const buffer = new Array(width * height).fill(" ")
+  const renderSettings = [
+    { indices: [1,2,3,4], letter: "a" },
+    { indices: [7,14], letter: "b" },
+    { indices: [12,19], letter: "c" },
+    { indices: [22,23,24,25], letter: "d" },
+    { indices: [28,35], letter: "e" },
+    { indices: [33,40], letter: "f" },
+    { indices: [43,44,45,46], letter: "g" },
+  ]
 
   for (var i = 0; i < segments.length; i++) {
     let on = segments[i]
@@ -19,15 +28,6 @@ function renderSegments(segments) {
   return buffer.join("")
 }
 
-const renderSettings = [
-  { indices: [1,2,3,4], letter: "a" },
-  { indices: [7,14], letter: "b" },
-  { indices: [12,19], letter: "c" },
-  { indices: [22,23,24,25], letter: "d" },
-  { indices: [28,35], letter: "e" },
-  { indices: [33,40], letter: "f" },
-  { indices: [43,44,45,46], letter: "g" },
-]
-const pixels = [ 1,1,1,1,1,1,1,1 ]
+const pixels = [ 1,1,1,1,1,1,1 ]
 
 console.log(renderSegments(pixels))
