f46124ca7e1403c67d6e4a9ac257632ecf9610cf
Steve Kane <kanesteven@gmail.com>
2021-12-20 20:18:01 -0600

    final details on 17 cleanup

--- 17/17.js
+++ 17/17.js
@@ -1,34 +1,20 @@
-const { hash, fold, map, contains, thereExists } = require("../utils")
-const { min, max } = Math
+const { max } = Math
 
-function step([[x,y],[vx,vy]]) {
-  return [
-    [x+vx,y+vy],
-    [vx === 0 ? vx : (vx < 0 ? vx+1 : vx-1),vy-1]
-  ]
-}
-
-function inBounds([[xmin,xmax],[ymin,ymax]],[x,y]) {
-  return x >= xmin && x <= xmax && y >= ymin && y <= ymax
-}
-
-function search(bounds) {
-  let [[xmin,xmax],[ymin,ymax]] = bounds
+function launchVelocities([[xmin,xmax],[ymin,ymax]]) {
   let bestHeight = 0
   let hits = []
 
   for (let i = 0; i <= xmax; i++) {
     for (let j = ymin; j <= -ymin; j++) {
       let thisHeight = 0
-      let s = [[0,0],[i,j]]
       let x = 0
       let y = 0
+      let vx = i
+      let vy = j
 
       while (true) {
-        x = s[0][0]
-        y = s[0][1]
         thisHeight = max(thisHeight,y)
-        if (inBounds(bounds,s[0])) {
+        if (x >= xmin && x <= xmax && y >= ymin && y <= ymax) {
           bestHeight = max(bestHeight,thisHeight)
           hits.push([i,j])
           break
@@ -37,7 +23,10 @@ function search(bounds) {
         } else if (y < ymin) {
           break
         }
-        s = step(s)
+        x += vx
+        y += vy
+        vx = vx === 0 ? 0 : vx < 0 ? vx+1 : vx-1
+        vy -= 1
       }
     }
   }
@@ -46,10 +35,8 @@ function search(bounds) {
 
 let exampleBounds = [[20,30],[-10,-5]]
 let inputBounds = [[111,161],[-154,-101]]
-let pt1 = search(exampleBounds)
-let pt2 = search(inputBounds)
+let [exampleMaxHeight,exampleVelocities] = launchVelocities(exampleBounds)
+let [inputMaxHeight,inputVelocities] = launchVelocities(inputBounds)
 
-console.log(pt1[0])
-console.log(pt1[1].length)
-console.log(pt2[0])
-console.log(pt2[1].length)
+console.log(`${exampleMaxHeight} among ${exampleVelocities.length} options`)
+console.log(`${inputMaxHeight} among ${inputVelocities.length} options`)
